1a3d8d995ac324d6e3055d7e312334ecbf684767
lesak215 <lesak215@gmail.com>
2026-01-01 18:59:40 +1000

    попытка2

---
+++ server/Models/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GameServer
+{
+    //состояние игры
+    public enum GameStatus
+    {
+        Waiting,
+        PlacingShips,
+        InProgress,
+        Finished
+    }
+    //кто ходит
+    public enum PlayerTurn
+    {
+        Player1,
+        Player2
+    }
+    //результат выстрела
+    public enum ShotResultType
+    {
+        Miss,
+        Hit,
+        Destroyed,
+        Invalid
+    }
+}
---
+++ server/Models/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    public class Player
+    {
+        public string Id { get; private set; }
+        public string Name { get; set; }
+        public TcpClient Client { get; set; }
+        public DateTime ConnectedAt { get; private set; }
+
+        public bool IsConnected
+        {
+            get
+            {
+                if (Client == null)
+                {
+                    return false;
+                }
+                return Client.Connected;
+            }
+        }
+
+        public Player()
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = "Player";
+            ConnectedAt = DateTime.UtcNow;
+        }
+    }
+}
